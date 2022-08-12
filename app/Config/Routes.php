2ff6e544bc0ae69f8135593ad77bd7654f0b436d
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-08-13 01:55:12 +0700

    rename route user to users

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -42,29 +42,29 @@ $routes->get('/', 'Home::index');
 $routes->get('sales/trash', 'Sales::trash'); //hapus sementara
 $routes->get('merk/trash', 'Merk::trash'); //hapus sementara
 $routes->get('barang/trash', 'Barang::trash'); //hapus sementara
-$routes->get('user/trash', 'User::trash'); //hapus sementara
+$routes->get('user/trash', 'Users::trash'); //hapus sementara
 
 $routes->get('sales/restore/(:any)', 'Sales::restore/$1'); //restore 1 data
 $routes->get('merk/restore/(:any)', 'Merk::restore/$1'); //restore 1 data
-$routes->get('user/restore/(:any)', 'User::restore/$1'); //restore 1 data
+$routes->get('user/restore/(:any)', 'Users::restore/$1'); //restore 1 data
 
 $routes->get('sales/restore', 'Sales::restore'); //restore all
 $routes->get('merk/restore', 'Merk::restore'); //restore all
-$routes->get('user/restore', 'User::restore'); //restore all
+$routes->get('user/restore', 'Users::restore'); //restore all
 
 $routes->delete('sales/hapus/(:any)', 'Sales::hapus/$1'); //hapus permanen 1 data
 $routes->delete('merk/hapus/(:any)', 'Merk::hapus/$1'); //hapus permanen 1 data
-$routes->delete('user/hapus/(:any)', 'User::hapus/$1'); //hapus permanen 1 data
+$routes->delete('user/hapus/(:any)', 'Users::hapus/$1'); //hapus permanen 1 data
 
 $routes->delete('/sales/hapus', 'Sales::hapus'); //hapus permanen all
 $routes->delete('merk/hapus', 'Merk::hapus'); //hapus permanen all
-$routes->delete('/user/hapus', 'User::hapus'); //hapus permanen all
+$routes->delete('/user/hapus', 'Users::hapus'); //hapus permanen all
 
 $routes->resource('sales');
 $routes->resource('merk');
 $routes->resource('kategori');
 $routes->resource('barang');
-$routes->resource('user');
+$routes->resource('users');
 
 
 
