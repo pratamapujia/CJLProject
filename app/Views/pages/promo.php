08c656b6723fc97f5547b23af8f6a8294f0a43e4
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-08-14 19:40:56 +0700

    fixed img promo dan newproducts page

--- app/Views/pages/promo.php
+++ app/Views/pages/promo.php
@@ -18,36 +18,36 @@
       </div>
       <div class="row justify-content-center" style="padding-bottom: 10px;">
           <div class="card col-md-3" style="width: 20rem; padding: 10px">
-              <img src="/img/Logo/weida.png" class="card-img-top" alt="WEIDA" />       
+              <img src="/assets/img/Logo/weida.png" class="card-img-top" alt="WEIDA" />       
           </div>
 
           <div class="card col-md-3" style="width: 20rem; padding: 10px">
-              <img src="/img/Logo/dixon.jpg" class="card-img-top" alt="DIXON" />
+              <img src="/assets/img/Logo/dixon.jpg" class="card-img-top" alt="DIXON" />
           </div>
 
           <div class="card col-md-3" style="width: 20rem; padding: 10px">
-              <img src="/img/Logo/fullmark.jpg" class="card-img-top" alt="FULLMARK" />
+              <img src="/assets/img/Logo/fullmark.jpg" class="card-img-top" alt="FULLMARK" />
           </div>
 
           <div class="card col-md-3" style="width: 20rem; padding: 10px">
-              <img src="/img/Logo/bosi tools.png" class="card-img-top" alt="BOSI TOOLS" />
+              <img src="/assets/img/Logo/bosi tools.png" class="card-img-top" alt="BOSI TOOLS" />
           </div>
       </div>
       <div class="row justify-content-center" style="padding-bottom: 10px;">
           <div class="card col-md-3" style="width: 20rem; padding: 10px">
-              <img src="/img/Logo/J&W.jpg" class="card-img-top" alt="J&W" />       
+              <img src="/assets/img/Logo/J&W.jpg" class="card-img-top" alt="J&W" />       
           </div>
 
           <div class="card col-md-3" style="width: 20rem; padding: 10px">
-              <img src="/img/Logo/Mitutoyo.png" class="card-img-top" alt="Mitutoyo" />
+              <img src="/assets/img/Logo/Mitutoyo.png" class="card-img-top" alt="Mitutoyo" />
           </div>
 
           <div class="card col-md-3" style="width: 20rem; padding: 10px">
-              <img src="/img/Logo/NACHI.jpg" class="card-img-top" alt="NACHI" />
+              <img src="/assets/img/Logo/NACHI.jpg" class="card-img-top" alt="NACHI" />
           </div>
 
           <div class="card col-md-3" style="width: 20rem; padding: 10px">
-              <img src="/img/Logo/fullmark.jpg" class="card-img-top" alt="Fullmark" />
+              <img src="/assets/img/Logo/fullmark.jpg" class="card-img-top" alt="Fullmark" />
           </div>
       </div>
     </div>
