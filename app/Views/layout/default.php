382ae71a69db8ca9857b825de0165e0f3ef73023
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-08-17 22:20:16 +0700

    update default.php

--- app/Views/layout/default.php
+++ app/Views/layout/default.php
@@ -134,9 +134,6 @@
       <div class="copyright">
         &copy; Copyright <strong><span>Cipta Jaya Lestari</span></strong>. All Rights Reserved
       </div>
-      <div class="credits">
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
     </div>
   </footer><!-- End Footer -->
 
