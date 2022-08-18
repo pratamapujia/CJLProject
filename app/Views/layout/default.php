e177f8f935e181acffb2186ba0a4378359f30edc
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-08-18 10:07:28 +0700

    revisi link rel syle

--- app/Views/layout/default.php
+++ app/Views/layout/default.php
@@ -31,8 +31,7 @@
 
   <!-- Template Main CSS File -->
   <link href="<?= base_url(); ?>/assets/landing/css/style.css" rel="stylesheet">
-  <link href="<?= base_url(); ?>/assets/css/style.css" rel="stylesheet">
-  <link href="<?= base_url(); ?>/assets/css/styles.css" rel="stylesheet">
+  
 
 </head>
 
