8fb2aa933918a11e6b5341c50670065dc0224ea9
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-08-18 09:22:34 +0700

    revisi directory assets

--- app/Views/layout/default.php
+++ app/Views/layout/default.php
@@ -18,12 +18,12 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="<?= base_url(); ?>/assets/landing/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?= base_url(); ?>/assets/landing/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="<?= base_url(); ?>/assets/landing/vendor/aos/aos.css" rel="stylesheet">
-  <link href="<?= base_url(); ?>/assets/landing/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="<?= base_url(); ?>/assets/landing/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-  <link href="<?= base_url(); ?>/assets/landing/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>/assets/landing/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>/assets/landing/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>/assets/landing/aos/aos.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>/assets/landing/remixicon/remixicon.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>/assets/landing/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>/assets/landing/glightbox/css/glightbox.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
   <link href="<?= base_url(); ?>/assets/landing/css/style.css" rel="stylesheet">
@@ -140,13 +140,13 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="<?= base_url(); ?>/assets/landing/vendor/bootstrap/js/bootstrap.bundle.js"></script>
-  <script src="<?= base_url(); ?>/assets/landing/vendor/aos/aos.js"></script>
-  <script src="<?= base_url(); ?>/assets/landing/vendor/php-email-form/validate.js"></script>
-  <script src="<?= base_url(); ?>/assets/landing/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="<?= base_url(); ?>/assets/landing/vendor/purecounter/purecounter.js"></script>
-  <script src="<?= base_url(); ?>/assets/landing/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="<?= base_url(); ?>/assets/landing/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="<?= base_url(); ?>/assets/landing/bootstrap/js/bootstrap.bundle.js"></script>
+  <script src="<?= base_url(); ?>/assets/landing/aos/aos.js"></script>
+  <script src="<?= base_url(); ?>/assets/landing/php-email-form/validate.js"></script>
+  <script src="<?= base_url(); ?>/assets/landing/swiper/swiper-bundle.min.js"></script>
+  <script src="<?= base_url(); ?>/assets/landing/purecounter/purecounter.js"></script>
+  <script src="<?= base_url(); ?>/assets/landing/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="<?= base_url(); ?>/assets/landing/glightbox/js/glightbox.min.js"></script>
 
   <!-- Template Main JS File -->
   <script src="<?= base_url(); ?>/assets/landing/js/main.js"></script>
