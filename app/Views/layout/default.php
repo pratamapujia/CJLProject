e52003b1b0d33a47c136d02e42bb8332e8ed2312
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-08-18 11:21:09 +0700

    nambah CDN bootstrap

--- app/Views/layout/default.php
+++ app/Views/layout/default.php
@@ -28,6 +28,8 @@
   <link href="<?= base_url(); ?>/assets/landing/remixicon/remixicon.css" rel="stylesheet">
   <link href="<?= base_url(); ?>/assets/landing/swiper/swiper-bundle.min.css" rel="stylesheet">
   <link href="<?= base_url(); ?>/assets/landing/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
   <!-- Template Main CSS File -->
   <link href="<?= base_url(); ?>/assets/landing/css/style.css" rel="stylesheet">
